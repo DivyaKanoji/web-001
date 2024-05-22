48e783c11f538d5976c7270348153bf540d79f49
DivyaKanoji <168465749+DivyaKanoji@users.noreply.github.com>
2024-05-22 20:10:20 +0530

    Add files via upload

---
+++ rectangle.js
@@ -0,0 +1,10 @@
+// function to find the area of rectangle
+function areaRectangle(a,b) {
+    let area = a*b;
+    return area;
+}
+
+// Driver program
+let a=3;
+let b=6;
+console.log("area" +areaRectangle(a,b));
