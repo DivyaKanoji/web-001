cc8f71bda6312d156e1bcb89f89a33310e1c9647
DivyaKanoji <168465749+DivyaKanoji@users.noreply.github.com>
2024-05-24 20:05:26 +0530

    Add files via upload

---
+++ for in.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Monica",
+    class: 7
+};
+
+// loop through the keys of student object
+for (let key in student) {
+
+    // display the key-value pairs
+    console.log(`${key} => ${student[key]}`);
+};
+
---
+++ for.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 3; i++) {
+    console.log("Hello, world!");
+}
+
+for (let i = 1; i < 6; i++) {
+    console.log(i);
+}
+
